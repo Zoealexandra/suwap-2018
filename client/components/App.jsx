3b7a22bf0211a558ffb502c1a9e016c394494796
sarah-mclaughlin <sarahmclaughlin0@gmail.com>
2018-05-26 10:14:32 +1200

    added home page

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+
+import Home from './Home'
+import Header from './Header'
+import Footer from './Footer'
+import Samoa from './Samoan'
 
 const App = () => {
   return (
-    <h1>React development has begun!</h1>
+    <Router>
+      <div className='app container'>
+        <Switch>
+          <Route path='/' component={Header} />
+          <Route path='/' component={Footer} />
+          <Route exact path='/' component={Home} />
+          <Route path='/samoa' component={Samoa} />
+        </Switch>
+      </div>
+    </Router>
   )
 }
 
 export default App
-
