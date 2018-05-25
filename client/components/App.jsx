101345a1010f07667d86dbdfcd4a383a603aae4c
sarah-mclaughlin <sarahmclaughlin0@gmail.com>
2018-05-26 11:07:38 +1200

    we have something on a page

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,21 +1,22 @@
 import React from 'react'
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+// import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import {HashRouter as Router, Route} from 'react-router-dom'
 
 import Home from './Home'
-import Header from './Header'
-import Footer from './Footer'
-import Samoa from './Samoan'
+// import Header from './Header'
+// import Footer from './Footer'
+import Samoan from './Samoan'
 
 const App = () => {
   return (
     <Router>
-      <div className='app container'>
-        <Switch>
-          <Route path='/' component={Header} />
-          <Route path='/' component={Footer} />
-          <Route exact path='/' component={Home} />
-          <Route path='/samoa' component={Samoa} />
-        </Switch>
+      <div className='app'>
+        {/* <Switch> */}
+        {/* <Route path='/' component={Header} />
+        <Route path='/' component={Footer} /> */}
+        <Route path='/' component={Home} />
+        <Route path='/samoan' component={Samoan} />
+        {/* </Switch> */}
       </div>
     </Router>
   )
--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,1 +1,18 @@
 import React from 'react'
+// import {Link} from 'react-router-dom'
+
+const Home = () => {
+  return (
+    <div className='people-list'>
+      <h1>Select the culture you most closely identify with:</h1>
+      {/* <ul>
+        <li key='samoa'>
+          <Link to='/samoa'>Samoa</Link>
+        </li>
+      </ul> */}
+    </div>
+  )
+}
+
+export default Home
+
